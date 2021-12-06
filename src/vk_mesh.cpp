bedc78d73eee8cd3dbf21231b9b3be98d6a385d4
junwide <junwide@outlook.com>
2021-12-07 00:37:23 +0800

    add support load obj file

--- src/vk_mesh.cpp
+++ src/vk_mesh.cpp
@@ -1,4 +1,6 @@
 #include <vk_mesh.h>
+#include <tiny_obj_loader.h>
+#include <iostream>
 
 VertexInputDescription Vertex::get_vertex_description()
 {
@@ -36,4 +38,69 @@ VertexInputDescription Vertex::get_vertex_description()
 	description.attributes.push_back(normalAttribute);
 	description.attributes.push_back(colorAttribute);
 	return description;
+}
+
+bool Mesh::load_from_obj(const char* filename)
+{
+	tinyobj::attrib_t attrib;
+	//shapes contains the info for each separate object in the file
+	std::vector<tinyobj::shape_t> shapes;
+	std::vector<tinyobj::material_t> materials;
+
+	std::string warn;
+	std::string err;
+	tinyobj::LoadObj(&attrib, &shapes, &materials, &warn, &err, filename, nullptr);
+	if (!warn.empty()) {
+		std::cout << "WARN: " << warn << std::endl;
+	}
+	//if we have any error, print it to the console, and break the mesh loading.
+	//This happens if the file can't be found or is malformed
+	if (!err.empty()) {
+		std::cerr << err << std::endl;
+		return false;
+	}
+	// SIMD
+	// Loop over shapes
+	for (size_t s = 0; s < shapes.size(); s++) {
+		// Loop over faces(polygon)
+		size_t index_offset = 0;
+		for (size_t f = 0; f < shapes[s].mesh.num_face_vertices.size(); f++) {
+
+			//hardcode loading to triangles
+			int fv = 3;
+
+			// Loop over vertices in the face.
+			for (size_t v = 0; v < fv; v++) {
+				// access to vertex
+				tinyobj::index_t idx = shapes[s].mesh.indices[index_offset + v];
+
+				//vertex position
+				tinyobj::real_t vx = attrib.vertices[3 * idx.vertex_index + 0];
+				tinyobj::real_t vy = attrib.vertices[3 * idx.vertex_index + 1];
+				tinyobj::real_t vz = attrib.vertices[3 * idx.vertex_index + 2];
+				//vertex normal
+				tinyobj::real_t nx = attrib.normals[3 * idx.normal_index + 0];
+				tinyobj::real_t ny = attrib.normals[3 * idx.normal_index + 1];
+				tinyobj::real_t nz = attrib.normals[3 * idx.normal_index + 2];
+
+				//copy it into our vertex
+				Vertex new_vert;
+				new_vert.position.x = vx;
+				new_vert.position.y = vy;
+				new_vert.position.z = vz;
+
+				new_vert.normal.x = nx;
+				new_vert.normal.y = ny;
+				new_vert.normal.z = nz;
+
+				//we are setting the vertex color as the vertex normal. This is just for display purposes
+				new_vert.color = new_vert.normal;
+
+
+				_vertices.push_back(new_vert);
+			}
+			index_offset += fv;
+		}
+	}
+	return true;
 }
